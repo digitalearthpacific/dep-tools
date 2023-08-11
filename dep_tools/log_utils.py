c3259d619f2c0ec8e39b78cdb3268082a39d1028
Jesse Anderson <jesse.anderson@gmail.com>
2023-08-11 10:16:28 -0700

    Removed stray file

--- dep_tools/log_utils.py
+++
@@ -1,2 +0,0 @@
-def get_log_path(prefix: str, dataset_id: str, version: str, datetime: str) -> str:
-    return f"{prefix}/{dataset_id}/logs/{dataset_id}_{version}_{datetime.replace('/', '_')}_log.csv"
