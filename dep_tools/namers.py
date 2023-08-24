f748710a241439b0ff5fe1e1661702db7fc5137d
Jesse Anderson <jesse.anderson@gmail.com>
2023-08-24 14:17:19 +1200

    code and id are subfolders

--- dep_tools/namers.py
+++ dep_tools/namers.py
@@ -23,13 +23,14 @@ class DepItemPath(ItemPath):
         self.version = self.version.replace(".", "-")
 
     def _folder(self, item_id) -> str:
+        if isinstance(item_id, List) or isinstance(item_id, Tuple):
+            item_id = "\\".join(item_id)
         return f"dep_{self.sensor}_{self.dataset_id}\\{self.version}\\{item_id}\\{self.time}"
 
     def _basename(self, item_id, asset_name) -> str:
+        if isinstance(item_id, List) or isinstance(item_id, Tuple):
+            item_id = "_".join(item_id)
         return f"dep_{self.sensor}_{self.dataset_id}_{item_id}_{self.time}_{asset_name}"
 
     def path(self, item_id, asset_name, ext=".tif") -> str:
-        if isinstance(item_id, List) or isinstance(item_id, Tuple):
-            item_id = "_".join(item_id)
-
         return f"{self._folder(item_id)}\\{self._basename(item_id, asset_name)}{ext}"
