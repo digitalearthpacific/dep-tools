5b28ce51d2065cac59090bebd42d272cdf9c0865
Jesse Anderson <jesse.anderson@gmail.com>
2024-07-29 14:03:00 -0700

    added proof of need for mspc modifier for stac search

--- tests/test_searchers.py
+++ tests/test_searchers.py
@@ -1,4 +1,5 @@
 import geopandas as gpd
+import odc.stac
 from pystac import ItemCollection
 import pytest
 from shapely.geometry import box
@@ -35,3 +36,11 @@ def test_PystacSearcher(area, mspc_catalog):
 def test_LandsatPystacSearcher_exclude_platforms(area):
     s = LandsatPystacSearcher(exclude_platforms=["landsat-7"])
     s.search(area)
+
+
+def test_unsigned_load(area, mspc_catalog):
+    s = PystacSearcher(
+        catalog=mspc_catalog, collections=["landsat-c2-l2"], datetime="2007"
+    )
+    items = s.search(area)
+    odc.stac.load([items[0]])
