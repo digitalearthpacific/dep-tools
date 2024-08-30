a586ee72fa3cb53f16005cb7b2e4d30a837946e6
Jesse Anderson <jesse.anderson@gmail.com>
2024-08-30 14:34:20 -0700

    added passing test

--- tests/test_antimeridian_fixes.py
+++ tests/test_antimeridian_fixes.py
@@ -1,11 +1,11 @@
 from geopandas import GeoDataFrame, read_file
+from numpy import isclose
 from shapely.geometry import LineString, Polygon
 import shapely.wkt
 
 from dep_tools.utils import (
     shift_negative_longitudes,
     bbox_across_180,
-    search_across_180,
 )
 
 
@@ -24,13 +24,14 @@ def test_shift_negative_longitudes_noncrossing_linestring():
 
 def test_bbox_across_180_crossing():
     crossing_polygon = Polygon(
-        [(179.0, 1.0), (-179.0, 1.0), (-179.0, -1.0), (179.0, -1.0), (179.0, 1.0)]
+        [(179.0, 1.0), (179.0, -1.0), (-179.0, -1.0), (-179.0, 1.0), (179.0, 1.0)]
     )
+
     crossing_gdf = GeoDataFrame(geometry=[crossing_polygon], crs=4326)
     bbox = bbox_across_180(crossing_gdf)
-    assert isinstance(bbox, tuple)
-    assert bbox[0] == [179.0, -1.0, 180, 1.0]
-    assert bbox[1] == [-180, -1.0, -179.0, 1.0]
+    assert isinstance(bbox, list | tuple)
+    assert bbox[0] == [179, -1, 180, 1]
+    assert bbox[1] == [-180, -1, -179, 1]
 
 
 def test_bbox_across_180_noncrossing():
@@ -41,6 +42,18 @@ def test_bbox_across_180_noncrossing():
     bbox = bbox_across_180(crossing_gdf)
     assert isinstance(bbox, list)
     assert bbox == [175.0, -1.0, 179.0, 1.0]
+
+
+def test_features_on_either_side_of_180():
+    features = read_file("tests/features_on_either_side.geojson")
+    bbox = bbox_across_180(features)
+    assert len(bbox) == 2
+    assert isclose(
+        bbox[0], [179.97098076944076, -19.044135782844712, 180, -18.555752850452095]
+    ).all()
+    assert isclose(
+        bbox[1], [-180, -19.044135782844712, -179.92643501801794, -18.555752850452095]
+    ).all()
 
 
 def test_geom_split_at_180():
@@ -56,3 +69,22 @@ def test_geom_split_at_180():
     assert isinstance(bbox, tuple)
     assert bbox[0] == [179.358, -18.2303, 180, -16.4619]
     assert bbox[1] == [-180, -18.2303, -178.577, -16.4619]
+
+
+def test_geom_tile_across_180():
+    # This is from tile 66,22 but with rounded coords
+    crossing_polygon = Polygon(
+        [
+            [179.9, -15.9],
+            [179.9, -16.8],
+            [180.8, -16.8],
+            [180.8, -15.9],
+            [179.9, -15.9],
+        ]
+    )
+    gdf = GeoDataFrame(geometry=[crossing_polygon], crs=4326)
+    bbox = bbox_across_180(gdf)
+
+    assert isinstance(bbox, tuple)
+    assert isclose(bbox[0], [179.9, -16.8, 180, -15.9]).all()
+    assert isclose(bbox[1], [-180, -16.8, -179.2, -15.9]).all()
